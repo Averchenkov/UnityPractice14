b99a1a495ea5bff5d5851cb3c36ee65034d81cdd
Averchenkov <nicroniys@gmail.com>
2020-05-26 15:15:54 +0300

    add p14

---
+++ pr14/Assets/Scripts/Interface.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interface : MonoBehaviour
+{
+    public GameObject boxPrefab;
+    public GameObject spherePrefab;
+    public GameObject panel;
+    public GameObject spawnObject;
+    private bool isActive;
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (!isActive)
+            {
+                panel.SetActive(true);
+                isActive = true;
+            }
+            else
+            {
+                panel.SetActive(false);
+                isActive = false;
+            }
+        }
+    }
+
+    public void CreateBox()
+    {
+        Instantiate(boxPrefab, spawnObject.transform.position, spawnObject.transform.rotation);
+    }
+    public void CreateSphere()
+    {
+        Instantiate(spherePrefab, spawnObject.transform.position, spawnObject.transform.rotation);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
